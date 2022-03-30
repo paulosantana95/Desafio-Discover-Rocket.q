17da3330b69a129260024d5bec36c169d4a79f46
Paulo Santana <paulosantana.sa@gmail.com>
2022-03-29 22:09:42 -0300

    Project execution and node configuration || Installation of EJS and express dependencies and installation of nodemon tool to facilitate development || All HTML has already been turned into .ejs || Creation of server.js and route.js scripts || Change of style and image folders to 'public' folder || Start of creating scripts for website functionality.

---
+++ public/scripts/main.js
@@ -0,0 +1,13 @@
+import { Modal } from './modal'
+
+const modal = Modal()
+
+//Pegar todos os botões com a classe 'check'
+//pegar o quando o marcar como lido for clicado
+
+
+
+
+
+//Abrir modal
+modal.open()
---
+++ public/scripts/modal.js
@@ -0,0 +1,17 @@
+module.exports = function Modal() {
+    function open() {
+        //funcionalidade de atribuir a classe active para a modal.
+
+    }
+
+    function close() {
+        //funcionalidade de remover a classe active para a modal.
+
+
+    }
+
+    return {
+        open,
+        close
+    }
+}
