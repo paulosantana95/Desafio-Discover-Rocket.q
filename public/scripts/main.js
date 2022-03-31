f153a30298e74b61f70528628d4d061e19d43754
Paulo Santana <paulosantana.sa@gmail.com>
2022-03-30 23:19:40 -0300

    building the script to open and close the 'modal' and change the layout of the 'modal' depending on the button clicked.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,13 +1,51 @@
-import { Modal } from './modal'
+import Modal from './modal.js'
 
-const modal = Modal()
+const modal = Modal();
+
+const modalTitle = document.querySelector('.modal h2');
+const modalDescription = document.querySelector('.modal p');
+const modalButton = document.querySelector('.modal button');
 
 //Pegar todos os botões com a classe 'check'
+
+const checkButtons = document.querySelectorAll(".actions a.check");
+
+checkButtons.forEach (button => {
+    //adicionar a escuta
+    button.addEventListener("click", handleClick);
+})
+
+
+// Quando botão delete for clicado ele irá abrir a modal.
+
+const deleteButton = document.querySelectorAll(".actions a.delete");
+
+deleteButton.forEach (button => {
+    //adicionar a escuta
+    button.addEventListener("click", (event) => handleClick(event, false));
+})
+
+function handleClick(event, check = true) {
+
+    event.preventDefault();
+
+    const text = check ? "Marcar como lido" : "Excluir"
+
+    modalTitle.innerHTML = `${text} esta pergunta`
+    modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+    modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red");
+
+
+    //abrir modal
+    modal.open();
+}
+
+
 //pegar o quando o marcar como lido for clicado
 
 
 
 
-
 //Abrir modal
-modal.open()
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,12 +1,19 @@
-module.exports = function Modal() {
+export default function Modal() {
+    
+    const modalWrapper = document.querySelector('.modal-wrapper')
+    const cancelButton = document.querySelector('.button.cancel');
+
+    cancelButton.addEventListener("click", close);
+    
     function open() {
         //funcionalidade de atribuir a classe active para a modal.
+        modalWrapper.classList.add("active");
 
     }
 
     function close() {
         //funcionalidade de remover a classe active para a modal.
-
+        modalWrapper.classList.remove("active");
 
     }
 
